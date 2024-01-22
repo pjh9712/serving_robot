bce06796e752a213c72430bc6540b5fa382d5a31
Jeong0806 <0806jsi@gmail.com>
2024-01-22 16:41:05 +0900

    feat: Fixed waypoints list

--- scripts/waypoint_publisher.py
+++ scripts/waypoint_publisher.py
@@ -8,11 +8,11 @@ from std_msgs.msg import Empty
 from geometry_msgs.msg import PoseWithCovarianceStamped
 
 # wp_id : (x, y)
-wp_list = {1 : (0.4, -0.7, 0.0, 0.0, 0.0),
-           2 : (5.0, -4.5, 0.0, 0.0, 1.6),
-           3 : (-6.3, -3.88, 0.0, 0.0, 1.6),
-           4 : (-12.3, -0.25, 0.0, 0.0, 1.6),
-           5 : (-9, -0.35, 0.0, 0.0, 3.14)}
+wp_list = {(0.4, -0.7, 0.0, 0.0, 0.0),
+           (5.0, -4.5, 0.0, 0.0, 1.6),
+           (-6.3, -3.88, 0.0, 0.0, 1.6),
+           (-12.3, -0.25, 0.0, 0.0, 1.6),
+           (-9, -0.35, 0.0, 0.0, 3.14)}
 
 def talker():
     pub_wp = rospy.Publisher('my_t3_waypoints', PoseWithCovarianceStamped, queue_size=1)
@@ -25,24 +25,11 @@ def talker():
     my_wp.header.stamp = rospy.Time.now()
     my_wp.header.frame_id = 'map'
 
-    init_x = -1.997662
-    init_y = -0.499985
-    init_roll = -0.000005
-    init_pitch = 0.000122
-    init_yaw = 0.010004
-
-    quaternion = tf.transformations.quaternion_from_euler(init_roll, init_pitch, init_yaw)
-
-    my_wp.pose.pose.orientation.x = quaternion[0]
-    my_wp.pose.pose.orientation.y = quaternion[1]
-    my_wp.pose.pose.orientation.z = quaternion[2]
-    my_wp.pose.pose.orientation.w = quaternion[3]
-
     for i in range(len(wp_list)):
         rospy.loginfo("Waypoint" + str(i))
-        quaternion = tf.transformations.quaternion_from_euler(wp_list[i+1][2], wp_list[i+1][3], wp_list[i+1][4])
-        my_wp.pose.pose.position.x = wp_list[i+1][0]
-        my_wp.pose.pose.position.y = wp_list[i+1][1]
+        quaternion = tf.transformations.quaternion_from_euler(wp_list[i][2], wp_list[i][3], wp_list[i][4])
+        my_wp.pose.pose.position.x = wp_list[i][0]
+        my_wp.pose.pose.position.y = wp_list[i][1]
         my_wp.pose.pose.orientation.x = quaternion[0]
         my_wp.pose.pose.orientation.y = quaternion[1]
         my_wp.pose.pose.orientation.z = quaternion[2]
